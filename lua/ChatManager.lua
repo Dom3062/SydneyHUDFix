9fac05f53007fa21f6d3879f99abca530b4b1532
Dominik Vyhňák <38251670+dominikvyhnak@users.noreply.github.com>
2018-10-16 15:58:45 +0200

    Rewrote ReceiveMessage function
    Because overriding the whole was not necessary. It also fixed issue with other mods not synchronizing correctly.

--- lua/ChatManager.lua
+++ lua/ChatManager.lua
@@ -4,18 +4,12 @@ local _layout_input_panel_original = ChatGui._layout_input_panel
 local key_press_original = ChatGui.key_press
 local close_original = ChatGui.close
 
-function ChatManager:_receive_message(channel_id, name, message, color, icon)
-	if not self._receivers[channel_id] then
-		return
-	end
-	local time = SydneyHUD._heist_time
-	for i, receiver in ipairs(self._receivers[channel_id]) do
-		if SydneyHUD:GetOption("show_heist_time") then
-			receiver:receive_message(time .. " " .. name, message, color, icon)
-		else
-			receiver:receive_message(name, message, color, icon)
-		end
-	end
+local _f_receive_message = ChatGui.receive_message
+function ChatGui:receive_message(name, message, color, icon)
+    if SydneyHUD:GetOption("show_heist_time") then
+        name = SydneyHUD._heist_time .. " " .. name
+    end
+    _f_receive_message(self, name, message, color, icon)
 end
 
 function ChatManager:receive_message_by_peer(channel_id, peer, message)
