ff17197393f744030cc09646857289f806d6e443
Dominik Vyhňák <38251670+dominikvyhnak@users.noreply.github.com>
2018-11-11 11:25:17 +0100

    Fixed "mods/SydneyHUD/lua/StageEndScreenGui.lua:9: attempt to compare number with nil" crash

--- lua/StageEndScreenGui.lua
+++ lua/StageEndScreenGui.lua
@@ -6,7 +6,7 @@ local SKIP_STAT_SCREEN_DELAY = SydneyHUD:GetOption("stat_screen_skip")
 
 function StageEndScreenGui:update(t, ...)
 	update_original(self, t, ...)
-	if not self._button_not_clickable and SKIP_STAT_SCREEN_DELAY >= 0 and SKIP_STAT_SCREEN then
+	if not self._button_not_clickable and SKIP_STAT_SCREEN_DELAY and SKIP_STAT_SCREEN_DELAY >= 0 and SKIP_STAT_SCREEN then
 		self._auto_continue_t = self._auto_continue_t or (t + SKIP_STAT_SCREEN_DELAY)
 		if t >= self._auto_continue_t then
 			managers.menu_component:post_event("menu_enter")
