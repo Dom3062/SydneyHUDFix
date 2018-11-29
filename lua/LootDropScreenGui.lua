0840145367c750dadf550bc9a4e8467bd4aaab57
Dominik Vyhňák <38251670+dominikvyhnak@users.noreply.github.com>
2018-11-29 18:17:34 +0100

    Fixed a crash
    Fixed "mods/SydneyHUD/lua/LootDropScreenGui.lua:14: attempt to compare number with nil" crash

--- lua/LootDropScreenGui.lua
+++ lua/LootDropScreenGui.lua
@@ -11,7 +11,7 @@ function LootDropScreenGui:update(t, ...)
 		self:_set_selected_and_sync(math.random(3))
 		self:confirm_pressed()
 	end
-	if not self._button_not_clickable and SKIP_LOOT_SCREEN_DELAY >= 0 and SKIP_LOOT_SCREEN then
+	if not self._button_not_clickable and SKIP_LOOT_SCREEN_DELAY and SKIP_LOOT_SCREEN_DELAY >= 0 and SKIP_LOOT_SCREEN then
 		self._auto_continue_t = self._auto_continue_t or (t + SKIP_LOOT_SCREEN_DELAY)
 		if t >= self._auto_continue_t then
 			self:continue_to_lobby()
