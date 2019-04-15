e3857f2b8c802c307cdd7013c57c01e97fd80056
hexco321 <hexco321@users.noreply.github.com>
2019-04-15 13:27:29 +0100

    Re-added missing press to cancel function and fixed press to interact appearing on blocked interactions

--- lua/lib/units/interactions/InteractionExt.lua
+++ lua/lib/units/interactions/InteractionExt.lua
@@ -1,4 +1,6 @@
 local set_tweak_data_original = BaseInteractionExt.set_tweak_data
+local interact_start_original = BaseInteractionExt.interact_start
+
 function BaseInteractionExt:set_tweak_data(...)
     local old_tweak = self.tweak_data
     local was_active = self:active()
@@ -13,4 +15,37 @@ local SecurityCameraInteractionExt_set_active_original = SecurityCameraInteracti
 function SecurityCameraInteractionExt:set_active(active, ...)
     managers.gameinfo:event("camera", "set_active", tostring(self._unit:key()), { active = active and true or false } )
     return SecurityCameraInteractionExt_set_active_original(self, active, ...)
+end
+
+function BaseInteractionExt:interact_start(player, data)
+	    if SydneyHUD:GetOption("push_to_interact") and self:can_interact(player) and tonumber(self:check_interact_time()) >= SydneyHUD:GetOption("push_to_interact_delay") and not self:_interact_blocked(player) then
+			local btn_cancel = SydneyHUD:GetOption("equipment_interrupt") and (managers.localization:btn_macro("use_item", true) or managers.localization:get_default_macro("BTN_USE_ITEM")) or (managers.localization:btn_macro("interact", true) or managers.localization:get_default_macro("BTN_INTERACT"))
+			managers.hud:show_interact({
+				text = managers.localization:text(self.tweak_data == "corpse_alarm_pager" and "sydneyhud_int_locked_pager" or "sydneyhud_int_locked", {BTN_CANCEL = btn_cancel}),
+				icon = self._tweak_data.icon,
+				force = true
+			})
+		end
+    return interact_start_original(self, player, data)
+end
+
+function BaseInteractionExt:check_interact_time()
+    local interact_timer = 0
+    if self:_timer_value() then
+        interact_timer = self:_get_timer()
+    end
+    if interact_timer < 10 then
+        if string.len(interact_timer) > 3 then
+            interact_timer = string.sub(math.round(interact_timer * 10) / 10, 1, 3)
+        elseif string.len(interact_timer) == 1 then
+            interact_timer = interact_timer .. ".0"
+        end
+    else
+        if string.len(interact_timer) > 4 then
+            interact_timer = string.sub(math.round(interact_timer * 10) / 10, 1, 4)
+        elseif string.len(interact_timer) == 2 then
+            interact_timer = interact_timer .. ".0"
+        end
+    end
+    return interact_timer
 end
