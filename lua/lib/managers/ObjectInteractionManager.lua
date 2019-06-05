609e93cc718130989dad426a8152aeceaa6e03f5
hexco321 <hexco321@users.noreply.github.com>
2019-06-05 09:50:07 +0100

    Re-added missing hold to pick function

--- lua/lib/managers/ObjectInteractionManager.lua
+++ lua/lib/managers/ObjectInteractionManager.lua
@@ -58,3 +58,17 @@ function ObjectInteractionManager:interact(...)
     
     return interact_original(self, ...)
 end
+
+local _update_targeted_original = ObjectInteractionManager._update_targeted
+function ObjectInteractionManager:_update_targeted(...)
+    _update_targeted_original(self, ...)
+    if SydneyHUD:GetOption("hold_to_pick") then
+        if alive(self._active_unit) then
+            local t = Application:time()
+            if self._active_unit:base() and self._active_unit:base().small_loot and managers.menu:get_controller():get_input_bool("interact") and (t >= (self._next_auto_pickup_t or 0)) then
+                self._next_auto_pickup_t = t + SydneyHUD:GetOption("hold_to_pick_delay")
+                self:interact(managers.player:player_unit())
+            end
+        end
+	end
+end
