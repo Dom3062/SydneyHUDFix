5eb4bbdec849fd3e6ead79cde2c58f3346464915
Dom <38251670+dominikvyhnak@users.noreply.github.com>
2019-06-15 14:39:16 +0200

    Fixed an issue where health bar is visible when you went to custody

--- lua/lib/managers/TradeManager.lua
+++ lua/lib/managers/TradeManager.lua
@@ -1,4 +1,5 @@
 Hooks:PostHook(TradeManager, 'on_player_criminal_death', "SydneyHUD:Criminal_Custody", function(self, criminal_name, respawn_penalty, hostages_killed, skip_netsend)
     SydneyHUD:Custody(LuaNetworking:LocalPeerID(), true)
     managers.money:ResetCivilianKills()
+    managers.hud:set_unit_health_visible(false)
 end)
