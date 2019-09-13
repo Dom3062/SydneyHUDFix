234ee60c8cf3a12cbded7d94e49ce2da3fb434d3
Dom <38251670+dominikvyhnak@users.noreply.github.com>
2019-09-13 21:13:53 +0200

    Improvements in HUDInteraction
    Improved getting interaction color and made the string translatable

--- lua/lib/managers/hud/HUDInteraction.lua
+++ lua/lib/managers/hud/HUDInteraction.lua
@@ -1,9 +1,4 @@
 local init_original = HUDInteraction.init
-local show_interaction_bar_original = HUDInteraction.show_interaction_bar
-local set_interaction_bar_width_original = HUDInteraction.set_interaction_bar_width
-local hide_interaction_bar_original = HUDInteraction.hide_interaction_bar
-local destroy_original = HUDInteraction.destroy
-
 function HUDInteraction:init(hud, child_name)
     init_original(self, hud, child_name)
     self._interact_timer_text = self._hud_panel:text({
@@ -43,6 +38,7 @@ function HUDInteraction:init(hud, child_name)
     self._bgtext4:set_x(self._bgtext4:x() + 1)
 end
 
+local show_interaction_bar_original = HUDInteraction.show_interaction_bar
 function HUDInteraction:show_interaction_bar(current, total)
     show_interaction_bar_original(self, current, total)
     self._interact_circle:set_visible(SydneyHUD:GetOption("show_interaction_circle"))
@@ -52,13 +48,14 @@ function HUDInteraction:show_interaction_bar(current, total)
     end
 end
 
+local set_interaction_bar_width_original = HUDInteraction.set_interaction_bar_width
 function HUDInteraction:set_interaction_bar_width(current, total)
     set_interaction_bar_width_original(self, current, total)
     if not self._interact_timer_text then
         return
     end
     local text = string.format("%.1f", total - current >= 0 and total - current or 0) .. "s"
-    local color = Color(SydneyHUD:GetOption("interaction_color_r"), SydneyHUD:GetOption("interaction_color_g"), SydneyHUD:GetOption("interaction_color_b"))
+    local color = SydneyHUD:GetColor("interaction_color")
     self._interact_timer_text:set_text(text)
     self._interact_timer_text:set_color(Color(
         color.a + (current / total),
@@ -71,6 +68,7 @@ function HUDInteraction:set_interaction_bar_width(current, total)
     end
 end
 
+local hide_interaction_bar_original = HUDInteraction.hide_interaction_bar
 function HUDInteraction:hide_interaction_bar(complete)
     hide_interaction_bar_original(self, complete and SydneyHUD:GetOption("show_interaction_circle"))
     self._interact_timer_text:set_visible(false)
@@ -79,6 +77,7 @@ function HUDInteraction:hide_interaction_bar(complete)
     end
 end
 
+local destroy_original = HUDInteraction.destroy
 function HUDInteraction:destroy()
     self._hud_panel:remove(self._hud_panel:child("interact_timer_text"))
     self._hud_panel:remove(self._hud_panel:child("bgtext1"))
@@ -96,7 +95,7 @@ function HUDInteraction:set_locked(status)
         end
 
         if SydneyHUD:GetOption("equipment_interrupt") then
-            self._hud_panel:child(self._child_name_text):set_text(managers.localization:to_upper_text("Press [G] to release"))
+            self._hud_panel:child(self._child_name_text):set_text(utf8.to_upper(managers.localization:text("equipment_interrupt_press")))
         end
     end
 end
