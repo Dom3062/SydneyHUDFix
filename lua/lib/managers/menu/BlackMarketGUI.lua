25e29c4a27b7aa013be66968d33c3577ff42142e
Dom <38251670+dominikvyhnak@users.noreply.github.com>
2019-09-16 22:35:09 +0200

    Added useful comment

--- lua/lib/managers/menu/BlackMarketGUI.lua
+++ lua/lib/managers/menu/BlackMarketGUI.lua
@@ -1,12 +1,15 @@
-local select_original = BlackMarketGuiSlotItem.select
-local deselect_original = BlackMarketGuiSlotItem.deselect
+--[[
+    Always show weapon mods in the Black Market
+]]
 
+local select_original = BlackMarketGuiSlotItem.select
 function BlackMarketGuiSlotItem:select(instant, no_sound)
     self._data.hide_unselected_mini_icons = false
     return select_original(self, instant, no_sound)
 end
 
+local deselect_original = BlackMarketGuiSlotItem.deselect
 function BlackMarketGuiSlotItem:deselect(instant)
     self._data.hide_unselected_mini_icons = false
-    return deselect_original(self, instant)
+    deselect_original(self, instant)
 end
