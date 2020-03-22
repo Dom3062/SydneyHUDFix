77284b19b50938b4e8d1071e49ed1cfd7c25cd46
Dom <38251670+dominikvyhnak@users.noreply.github.com>
2020-03-22 18:07:56 +0100

    Added commented function
    Will be used in later update

--- lua/lib/managers/hud/HUDAssaultCorner_AssaultStates.lua
+++ lua/lib/managers/hud/HUDAssaultCorner_AssaultStates.lua
@@ -16,6 +16,21 @@ function GroupAIStateBase:GetAssaultState()
     return self._task_data.assault.phase
 end
 
+--[[if Global.game_settings.level_id == "pbr2" and BAI.IsHost then
+    BAI:Hook(GroupAIStateBesiege, "_upd_recon_tasks", function(self)
+        if self._task_data.recon.tasks and self._task_data.recon.tasks[1] then
+            managers.hud:UpdateAssaultState("control")
+            BAI:SyncAssaultState("control", false, true, true)
+            BAI:Unhook(nil, "_upd_recon_tasks")
+        end
+    end)
+else
+    BAI:Hook(GroupAIStateBase, "on_enemy_weapons_hot", function(self)
+        managers.hud:UpdateAssaultState("control")
+        BAI:SyncAssaultState("control", false, true, true)
+    end)
+end]]
+
 SydneyHUD:Hook(GroupAIStateBase, "on_enemy_weapons_hot", function(self)
     managers.hud._hud_assault_corner:UpdateAssaultState("control")
     SydneyHUD:SyncAssaultState("control", false, true, true)
